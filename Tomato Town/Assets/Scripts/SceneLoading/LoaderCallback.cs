ea6c9eb372ba2641346578608a067e225e36d6ea
HardwinBui <buihardwin@yahoo.com>
2021-11-08 00:25:23 -0800

    Revert "Revert "started scene loading""
    This reverts commit 38fcf621a507efd97482d37af57669696edb3176.

---
+++ Tomato Town/Assets/Scripts/SceneLoading/LoaderCallback.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used to update the loading screen once
+public class LoaderCallback : MonoBehaviour {
+
+    private bool isLoaded;
+
+    // Update is called once per frame
+    private void Update() {
+        if (!isLoaded) {
+            isLoaded = true;
+            SceneLoader.LoaderCallback();
+        }        
+    }
+}
