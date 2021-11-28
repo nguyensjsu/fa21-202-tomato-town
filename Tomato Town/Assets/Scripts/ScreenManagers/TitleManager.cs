ad3e5092f655c3a07204bd78f7192fe3ec9cde5c
kejyang <k.yang256@gmail.com>
2021-11-27 23:45:44 -0800

    Added Title Screen

---
+++ Tomato Town/Assets/Scripts/ScreenManagers/TitleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+
+    public void NewGame()
+    {
+        SceneLoader.Load(SceneLoader.Scene.CombatArea);
+    }
+
+    public void QuitGame()
+    {
+ 
+        Application.Quit();
+    }
+}
